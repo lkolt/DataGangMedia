f92ace5744fbe4ec3c6edf6b49e1e77aba65c299
Alexey Korelin <alex.korelin.dev@gmail.com>
2019-09-28 01:17:53 +0300

    vk start

--- VCBot/sources.js
+++ VCBot/sources.js
@@ -1,11 +1,23 @@
 'use strict'
 
+const vk = require('./apis/vk')
+
 class sources {
+    constructor() {
+        this.vk = vk.get();
+    }
+
     get_posts () {
         return [{
             title: 'Awesome!',
             text: 'Wow!',
         }]
+    }
+
+    async get_vk_posts () {
+        const posts = await this.vk.get_posts() 
+
+        return posts
     }
 }
 
