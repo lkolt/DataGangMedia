605063fc2e3a85ba49c8390bb4fe8d0a8970f158
lkolt <lkolt2@mail.ru>
2019-09-29 00:58:25 +0300

    [VCBot] fix already posted

--- VCBot/main.js
+++ VCBot/main.js
@@ -14,7 +14,7 @@ class bot {
         this.sources = sources.get()
         this.wrapper = ml_wrapper.get()
         this.idx = 0
-        this.get_already_posted = this.get_already_posted()
+        this.get_posted = this.get_already_posted()
         this.get_posts()
         setInterval(this.get_posts.bind(this), 1000 * 60 * 30)
         setInterval(this.run.bind(this), 1000 * 60 * 10)
@@ -28,12 +28,12 @@ class bot {
     
     async get_post () {
         let posts = await this.posts
-        let alredy = await this.get_already_posted
+        let already = await this.get_posted
         while (this.idx < posts.length) {
             let post = posts[this.idx++]
             post['md5'] = md5(post['title'])
             this.idx++
-            if (!alredy.includes(post['md5'])){
+            if (!already.includes(post['md5'])){
                 return [post]
             }
         }
@@ -60,6 +60,8 @@ class bot {
             let formatted_posts = await this.wrapper.proccess(new_posts)
             console.log('Formatted posts:', formatted_posts.length)
             for (let post of formatted_posts) {
+                console.log(post.class)
+                // return
                 let res = this.api.create_entry(
                     post.title,
                     (
@@ -75,6 +77,7 @@ class bot {
                 } else {
                     console.log('Post', post, 'created')
                     this.new_post(post['md5'])
+                    this.get_posted.push(post['md5'])
                 }
             }
         }
