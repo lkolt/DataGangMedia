6778aea2e45ae9833f716266bb106792eb6c52f6
lkolt <lkolt2@mail.ru>
2019-09-29 02:08:12 +0300

    [ML] classification realized

--- ML/server.py
+++ ML/server.py
@@ -3,6 +3,7 @@ import json
 import kmeans_summarizer
 
 summurizer = kmeans_summarizer.Summarizer()
+classifier = kmeans_summarizer.Classifier()
 
 clients = []
 class SimpleChat(WebSocket):
@@ -10,8 +11,10 @@ class SimpleChat(WebSocket):
         print(self.data)
         js = json.loads(self.data)
         new_text = summurizer.get_summary(js['text'])
+        new_class = classifier.get_class(js['text'])
         new_data = js
         new_data['text'] = new_text
+        new_data['class'] = new_class
         print(f'Summurize:{new_data}')
         for client in clients:
             client.sendMessage(json.dumps(new_data))
