eae6aa20dfc2051c73312a6ac286673e2dc12150
lkolt <lkolt2@mail.ru>
2019-09-28 05:28:52 +0300

    [ML] summarizer MVC

--- ML/kmeans_summarizer.py
+++ ML/kmeans_summarizer.py
@@ -11,45 +11,52 @@ import re
 
 class Summarizer:
 
-    def preprocessing_rus(sentence):
+    def preprocessing_rus(self, sentence):
         sentence = sentence.lower()
         sentence = re.sub('[^А-Яа-яA-Za-z]+', ' ', sentence).strip()
         return sentence
 
-    def mean_sent_vector(sentence_tokens):
-        return np.array([np.mean([pretrained_ft[w] for w in sentence_tokens
-                                                    if w in pretrained_ft]
+    def mean_sent_vector(self, sentence_tokens):
+        return np.array([np.mean([self.pretrained_ft[w] for w in sentence_tokens
+                                                    if w in self.pretrained_ft]
                                  or [np.zeros(300)], axis=0)])
 
-    def get_summary(text: str):
+    def __init__(self):
+        print('Loading model...')
+        self.pretrained_ft = gensim.models.fasttext.load_facebook_model('models/ru.bin', encoding='utf-8')
+        print('Model loaded!')
+
+    def get_summary(self, text: str):
+        print('init model:', text)
+
         sents = sent_tokenize(text)
         sents_storage = deepcopy(sents)
         n_sents = int(round(np.sqrt(len(sents_storage))))
 
+        print('Before:', sents)
         for i in range(len(sents)):
-            sents[i] = preprocessing_rus(sents[i])
+            sents[i] = self.preprocessing_rus(sents[i])
             sents[i] = word_tokenize(sents[i])
             sents[i] = [word for word in sents[i] if word not in stopwords.words('russian')]
             for j in range(len(sents[i])):
                 sents[i][j] = pymorphy2.MorphAnalyzer().parse(sents[i][j])[0].normal_form
-
-        pretrained_ft = gensim.models.fasttext.load_facebook_model('ru.bin', encoding='utf-8')
+        print('After:', sents)
 
         sent_vectors = []
         for sent in sents:
-            sent_vectors.append(np.squeeze(mean_sent_vector(sent)))
+            sent_vectors.append(np.squeeze(self.mean_sent_vector(sent)))
         sent_vectors = np.asarray(sent_vectors)
-
+        print(len(sent_vectors))
         kmeans = KMeans(n_clusters=n_sents).fit(sent_vectors)
-
+        print(kmeans)
         centre_sents_idx = []
-        for j in range(n_clusters):
+        for j in range(n_sents):
             idx_cand = np.where(kmeans.labels_ == j)
             vec_cand = sent_vectors[idx_cand]
             closest, dist = pairwise_distances_argmin_min([kmeans.cluster_centers_[j]], vec_cand)
             centre_sents_idx.append(idx_cand[0][int(closest)])
         centre_sents_idx = sorted(centre_sents_idx)
-
+        print(centre_sents_idx)
         summary = ' '.join([sents_storage[i] for i in centre_sents_idx])
-
+        print(summary)
         return summary
--- ML/server.py
+++ ML/server.py
@@ -1,11 +1,20 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import json
+import kmeans_summarizer
+
+summurizer = kmeans_summarizer.Summarizer()
 
 clients = []
 class SimpleChat(WebSocket):
     def handleMessage(self):
         print(self.data)
+        js = json.loads(self.data)
+        new_text = summurizer.get_summary(js['text'])
+        new_data = js
+        new_data['text'] = new_text
+        print(f'Summurize:{new_data}')
         for client in clients:
-            client.sendMessage(self.data)
+            client.sendMessage(json.dumps(new_data))
 
     def handleConnected(self):
        print(self.address, 'connected')
