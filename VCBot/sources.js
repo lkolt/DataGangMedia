47e78e0eb4b7a5bf9a5904c61f479aef61b281ae
lkolt <lkolt2@mail.ru>
2019-09-28 17:27:03 +0300

    [VCBot] creating awesome titles

--- VCBot/sources.js
+++ VCBot/sources.js
@@ -2,6 +2,14 @@
 
 const vk = require('./apis/vk')
 
+const awesome_titles = ['Прекрасное краткое содержание!', 'Это важно!', 'Look at me', 'Дайджест',
+                        'Новый пост', 'Важные новости']
+
+let get_random_title = () => {
+    let idx = Math.floor(Math.random() * awesome_titles.length)
+    return awesome_titles[idx]
+}
+
 class sources {
     constructor() {
         this.vk = vk.get();
@@ -9,10 +17,12 @@ class sources {
 
     async get_posts () {
         let vk_posts = await this.vk.get_posts() 
-        // console.log(vk_posts)
+        console.log(vk_posts)
         let approved_posts = vk_posts.filter((item) => { return item.text.length  > 500 })
         approved_posts = approved_posts.map((item) => { 
-            item.title = 'Прекрасное краткое содержание!'
+            if (item.title === item.text) {
+                item.title = get_random_title()
+            }
             return item
         })
         
